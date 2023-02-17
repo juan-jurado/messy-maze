07decb4fb8f5017a62dcc539b00ff431f00333ad
Juan Jurado <juan-jurado@juan-jurado.local>
2023-02-17 12:54:28 -0500

    feature: add some comments

--- src/services/mazeService.js
+++ src/services/mazeService.js
@@ -8,20 +8,26 @@ function shortestPath(maze, startX, startY, endX, endY) {
   const visited = new Set([`${startX},${startY}`]);
 
   while (queue.length > 0) {
+    //Shift (remove) the first element of the array:
     const currentNode = queue.shift();
 
+    //verifica si currentNode es el nodo final
     if (currentNode.isSame(endNode)) {
+      // reconstruccion de la ruta mas corta
+
       const path = [];
       let current = currentNode;
+
       while (current !== null) {
         path.unshift([current.x, current.y]);
         current = current.parent;
       }
       return path;
     }
-
+    //currentNode is not the endNode
     const neighbors = getNeighbors(maze, currentNode);
     for (const neighbor of neighbors) {
+      //has()	Returns true if a value exists
       if (!visited.has(`${neighbor.x},${neighbor.y}`)) {
         visited.add(`${neighbor.x},${neighbor.y}`);
         neighbor.parent = currentNode;
@@ -30,7 +36,7 @@ function shortestPath(maze, startX, startY, endX, endY) {
     }
   }
 
-  return null;
+  return null; // no path found
 }
 
 module.exports = { shortestPath };
